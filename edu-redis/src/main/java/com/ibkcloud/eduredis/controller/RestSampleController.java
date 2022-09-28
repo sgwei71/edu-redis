e8ed650a9db69aa8db333cc569df510f44f74989
sgwei71 <sgwei71@gmail.com>
2022-09-28 14:11:43 +0900

    2nd Commit

--- edu-redis/src/main/java/com/ibkcloud/eduredis/controller/RestSampleController.java
+++ edu-redis/src/main/java/com/ibkcloud/eduredis/controller/RestSampleController.java
@@ -1,37 +1,44 @@
 package com.ibkcloud.eduredis.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;	
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestSampleController {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(RestSampleController.class);
-	
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
-	
+
+	@GetMapping("/")
+	public String healthCheck() {
+		return "OK";
+	}
+
+	@GetMapping("/getSessionValue")
+	@PostMapping("/getSessionValue")
+	public String getSessionValue(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		log.info("session id [{}]", session.getId());
+		return (String) session.getAttribute("key1");
+	}
+
 	@GetMapping("/getSessionId")
+	@PostMapping("/getSessionId")
 	public String getSessionId(HttpSession session) {
-		
-		session.setAttribute("key1","abcde");
+		session.setAttribute("key1", "abcde");
 		log.debug(session.getId());
 		return session.getId();
 	}
-	
-	@GetMapping("/geValue")
+
+	@GetMapping("/getValue")
 	public String getValue(HttpSession session) {
-		
 		log.debug(session.getId());
-		log.debug ((String)session.getAttribute("key1"));
-		
-		return (String)session.getAttribute("key1");
+		log.debug((String) session.getAttribute("key1"));
+		return (String) session.getAttribute("key1");
 	}
 }
